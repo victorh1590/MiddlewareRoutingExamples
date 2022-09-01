0bc7bf94fc91ca3ba46517c9a39355acb084983d
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-01 12:05:28 -0300

    Middleware branches, short-circuit, terminal.

---
+++ Middleware.cs
@@ -0,0 +1,31 @@
+namespace Platform
+{
+  public class QueryStringMiddleWare
+  {
+    private RequestDelegate? next;
+    public QueryStringMiddleWare()
+    {
+      // do nothing
+    }
+    public QueryStringMiddleWare(RequestDelegate nextDelegate)
+    {
+      next = nextDelegate;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+      if (context.Request.Method == HttpMethods.Get
+      && context.Request.Query["custom"] == "true")
+      {
+        if (!context.Response.HasStarted)
+        {
+          context.Response.ContentType = "text/plain";
+        }
+        await context.Response.WriteAsync("Class-based Middleware \n");
+      }
+      if (next != null)
+      {
+        await next(context);
+      }
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) =>
+app.Use(async (context, next) => // Modifies after calling next().
+{
+  await next();
+  await context.Response
+  .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) => // Short-circuit middleware.
+{
+  if (context.Request.Path == "/short")
+  {
+    await context.Response
+    .WriteAsync($"Request Short Circuited");
+  }
+  else
+  {
+    await next();
+  }
+});
+
+app.Use(async (context, next) => // Custom middleware.
 {
   if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
   {
@@ -10,6 +30,27 @@ app.Use(async (context, next) =>
   }
   await next();
 });
+
+((IApplicationBuilder)app).Map("/branch", branch => // Pipeline branch.
+{
+  // branch.UseMiddleware<Platform.QueryStringMiddleWare>();
+  // branch.Run(async (context) => // Terminal middleware.
+  //   {
+  //     await context.Response.WriteAsync($"Branch Middleware");
+  //   });
+  branch.Run(new Platform.QueryStringMiddleWare().Invoke); // Terminal class-middleware.
+});
+
+app.MapWhen(context => context.Request.Query.Keys.Contains("branch"), // Branching with predicates.
+  branch =>
+  {
+    branch.Use(async (HttpContext context, Func<Task> next) =>
+    {
+      await context.Response.WriteAsync($"Branch with predicates.");
+    });
+  });
+
+app.UseMiddleware<Platform.QueryStringMiddleWare>(); // Class middleware example.
 
 app.MapGet("/", () => "Hello World!");
 
