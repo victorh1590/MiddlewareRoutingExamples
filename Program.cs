4a591ed7b4582def49925f91ad2e55fad7864b91
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-01 17:40:24 -0300

    Bad routing technique example.

--- Program.cs
+++ Program.cs
@@ -3,10 +3,10 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<MessageOptions>(options =>
-{
-  options.CityName = "Albany";
-});
+// builder.Services.Configure<MessageOptions>(options =>
+// {
+//   options.CityName = "Albany";
+// });
 
 var app = builder.Build();
 
@@ -74,10 +74,17 @@ var app = builder.Build();
 //   await next();
 // });
 
-app.UseMiddleware<QueryStringMiddleWare>();
+// app.UseMiddleware<QueryStringMiddleWare>();
 
-app.UseMiddleware<LocationMiddleware>();
+// app.UseMiddleware<LocationMiddleware>();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+
+app.UseMiddleware<Population>();
+app.UseMiddleware<Capital>();
+app.Run(async (context) =>
+{
+  await context.Response.WriteAsync("Terminal Middleware Reached");
+});
 
 app.Run();
