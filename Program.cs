c23521f709ac5043a2a804662b04699f0a613580
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-01 18:44:03 -0300

    Using route variables.

--- Program.cs
+++ Program.cs
@@ -5,21 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseMiddleware<Population>();
-app.UseMiddleware<Capital>();
-
-// app.MapGet("routing", async context =>
-//   {
-//     await context.Response.WriteAsync("Request Was Routed1");
-//   });
-
-app.MapGet("routing", async context => {
- await context.Response.WriteAsync("Request Was Routed");
+app.MapGet("{first}/{second}/{third}", async context =>
+{
+  await context.Response.WriteAsync("Request Was Routed\n");
+  foreach (var kvp in context.Request.RouteValues)
+  {
+    await context.Response
+    .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+  }
 });
 app.MapGet("capital/uk", new Capital().Invoke);
 app.MapGet("population/paris", new Population().Invoke);
-//app.Run(async (context) => {
-// await context.Response.WriteAsync("Terminal Middleware Reached");
-//});
 
 app.Run();
