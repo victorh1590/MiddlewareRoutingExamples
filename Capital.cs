217f545f94ebd75c83c204180574a719497e04da
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-01 19:17:49 -0300

    Population and Capital are now endpoints and rely on the Routing Middleware to resolve the URLs.

---
+++ Capital.cs
@@ -0,0 +1,32 @@
+namespace Platform
+{
+  public class Capital
+  {
+    public static async Task Endpoint(HttpContext context)
+    {
+      string? capital = null;
+      string? country = context.Request.RouteValues["country"] as string;
+      switch ((country ?? "").ToLower())
+      {
+        case "uk":
+          capital = "London";
+          break;
+        case "france":
+          capital = "Paris";
+          break;
+        case "monaco":
+          context.Response.Redirect($"/population/{country}");
+          return;
+      }
+      if (capital != null)
+      {
+        await context.Response
+        .WriteAsync($"{capital} is the capital of {country}");
+      }
+      else
+      {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+      }
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ app.MapGet("{first}/{second}/{third}", async context =>
     .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
   }
 });
-app.MapGet("capital/uk", new Capital().Invoke);
-app.MapGet("population/paris", new Population().Invoke);
+app.MapGet("capital/{country}", Capital.Endpoint);
+app.MapGet("population/{city}", Population.Endpoint);
 
 app.Run();
