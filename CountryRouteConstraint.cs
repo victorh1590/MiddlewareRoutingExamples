ffa916241bf3d9ed3da7fb6018221e66b8c15121
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-03 20:01:59 -0300

    Routing ambiguity and how to fix it.

--- CountryRouteConstraint.cs
+++ CountryRouteConstraint.cs
@@ -4,8 +4,7 @@ namespace Platform
   {
     private static string[] countries = { "uk", "france", "monaco" };
     public bool Match(HttpContext? httpContext, IRouter? route,
-    string routeKey, RouteValueDictionary values,
-    RouteDirection routeDirection)
+    string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
     {
       string segmentValue = values[routeKey] as string ?? "";
       return Array.IndexOf(countries, segmentValue.ToLower()) > -1;
--- Program.cs
+++ Program.cs
@@ -3,20 +3,23 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<RouteOptions>(opts =>
+{
+  opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
+});
+
 var app = builder.Build();
 
-app.MapGet("{first:alpha:length(3)}/{second:bool}", async context =>
+app.Map("{number:int}", async context =>
 {
-  await context.Response.WriteAsync("Request Was Routed\n");
-  foreach (var kvp in context.Request.RouteValues)
-  {
-    await context.Response
-    .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-  }
-});
-app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
-app.MapGet("size/{city?}", Population.Endpoint)
-  .WithMetadata(new RouteNameMetadata("population")); // Route is named population.
+  await context.Response.WriteAsync("Routed to the int endpoint");
+}).Add(b => ((RouteEndpointBuilder)b).Order = 1);
+
+app.Map("{number:double}", async context =>
+{
+  await context.Response
+  .WriteAsync("Routed to the double endpoint");
+}).Add(b => ((RouteEndpointBuilder)b).Order = 2);
 
 app.MapFallback(async context =>
 {
