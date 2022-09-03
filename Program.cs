738552f2fb9f7b1367a599554ae07e1254520499
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-03 19:27:03 -0300

    Constraints are used to narrow down the set of URLs matched.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("{first}/{second}/{*catchall}", async context =>
+app.MapGet("{first:int}/{second:bool}", async context =>
 {
   await context.Response.WriteAsync("Request Was Routed\n");
   foreach (var kvp in context.Request.RouteValues)
