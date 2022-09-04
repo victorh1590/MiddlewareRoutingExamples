b30bb464e150310abd904563bfd05e4ce0c59eb4
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-03 21:19:51 -0300

    Dependency Injection Setup examples.

---
+++ Services/IResponseFormatter.cs
@@ -0,0 +1,7 @@
+namespace Platform.Services
+{
+  public interface IResponseFormatter
+  {
+    Task Format(HttpContext context, string content);
+  }
+}
---
+++ WeatherEndpoint.cs
@@ -0,0 +1,11 @@
+namespace Platform
+{
+  public class WeatherEndpoint
+  {
+    public static async Task Endpoint(HttpContext context)
+    {
+      await context.Response
+      .WriteAsync("Endpoint Class: It is cloudy in Milan");
+    }
+  }
+}
