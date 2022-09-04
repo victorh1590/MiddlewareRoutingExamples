2e9c2f53f9bf6878791c71ced0872a709eb1b5fa
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-03 22:18:15 -0300

    Resolving Service dependencies for middlewares and endpoints.

--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -1,11 +1,15 @@
+using Platform.Services;
+
 namespace Platform
 {
   public class WeatherEndpoint
   {
     public static async Task Endpoint(HttpContext context)
     {
-      await context.Response
-      .WriteAsync("Endpoint Class: It is cloudy in Milan");
+      IResponseFormatter formatter = // Resolves dependency everytime something is routed to this endpoint.
+        context.RequestServices.GetRequiredService<IResponseFormatter>();
+      await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
+
     }
   }
 }
--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -1,18 +1,21 @@
+using Platform.Services;
+
 namespace Platform
 {
   public class WeatherMiddleware
   {
     private RequestDelegate next;
-    public WeatherMiddleware(RequestDelegate nextDelegate)
+    private IResponseFormatter formatter;
+    public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter responseFormatter)
     {
       next = nextDelegate;
+      formatter = responseFormatter;
     }
     public async Task Invoke(HttpContext context)
     {
       if (context.Request.Path == "/middleware/class")
       {
-        await context.Response
-        .WriteAsync("Middleware Class: It is raining in London");
+        await formatter.Format(context, "Middleware Class: It is raining in London");
       }
       else
       {
