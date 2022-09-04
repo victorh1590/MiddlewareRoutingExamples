bbed1c1e0ed998506eab1fe7f2ee769abcd92db0
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-04 15:20:44 -0300

    Resolving Transient for every request in method signature instead.

--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -4,12 +4,12 @@ namespace Platform
 {
   public class WeatherEndpoint
   {
-    private IResponseFormatter formatter;
-    public WeatherEndpoint(IResponseFormatter responseFormatter)
-    {
-      formatter = responseFormatter;
-    }
-    public async Task Endpoint(HttpContext context)
+    // private IResponseFormatter formatter;
+    // public WeatherEndpoint(IResponseFormatter responseFormatter)
+    // {
+    //   formatter = responseFormatter;
+    // }
+    public async Task Endpoint(HttpContext context, IResponseFormatter formatter)
     {
       await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
     }
