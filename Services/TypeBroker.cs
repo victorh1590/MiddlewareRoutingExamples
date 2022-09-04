ceafecdbe37944e9f79ca09c26cc5ac69c4fbe82
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-03 21:33:11 -0300

    Example of usage of Type Broker.

--- Services/TypeBroker.cs
+++ Services/TypeBroker.cs
@@ -2,7 +2,7 @@ namespace Platform.Services
 {
   public static class TypeBroker
   {
-    private static IResponseFormatter formatter = new TextResponseFormatter();
+    private static IResponseFormatter formatter = new HtmlResponseFormatter();
     public static IResponseFormatter Formatter => formatter;
   }
 }
