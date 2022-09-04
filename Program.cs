e5bbcf26f6ed64af8f8479de728df0aa72de6c01
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-04 20:50:58 -0300

    Serving static files. Setting static files middleware options.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
 using Platform;
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Ref https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-logging/?view=aspnetcore-6.0
-builder.Services.AddHttpLogging(opts => {
- opts.LoggingFields = 
- HttpLoggingFields.RequestMethod | 
- HttpLoggingFields.RequestPath | 
- HttpLoggingFields.ResponseStatusCode;
+builder.Services.AddHttpLogging(opts =>
+{
+  opts.LoggingFields =
+  HttpLoggingFields.RequestMethod |
+  HttpLoggingFields.RequestPath |
+  HttpLoggingFields.ResponseStatusCode;
 });
 
 var app = builder.Build();
 
 app.UseHttpLogging();
+
+var env = app.Environment;
+
+app.UseStaticFiles();
+
+// Handles Requests to static content.
+app.UseStaticFiles(new StaticFileOptions // Config Static Files Middleware.
+{
+  FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
+  RequestPath = "/files"
+});
 
 //var logger = app.Services
 // .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
