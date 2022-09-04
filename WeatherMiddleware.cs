4ca2d1cfa7ce5e11430437e24692719804558335
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-04 15:26:03 -0300

    Transient example: each dependency in WeatherMiddleware is resolved by different instance.

--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -2,20 +2,23 @@ using Platform.Services;
 
 namespace Platform
 {
-  public class WeatherMiddleware
+  public class WeatherMiddleware // Scoped Service example. 
   {
     private RequestDelegate next;
     // private IResponseFormatter formatter;
-    public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter responseFormatter)
+    public WeatherMiddleware(RequestDelegate nextDelegate)
     {
       next = nextDelegate;
       // formatter = responseFormatter;
     }
-    public async Task Invoke(HttpContext context, IResponseFormatter formatter)
+    public async Task Invoke(HttpContext context, IResponseFormatter formatter1,
+    IResponseFormatter formatter2, IResponseFormatter formatter3) // 3 Dependencies on same service.
     {
       if (context.Request.Path == "/middleware/class")
       {
-        await formatter.Format(context, "Middleware Class: It is raining in London");
+        await formatter1.Format(context, string.Empty);
+        await formatter2.Format(context, string.Empty);
+        await formatter3.Format(context, string.Empty);
       }
       else
       {
