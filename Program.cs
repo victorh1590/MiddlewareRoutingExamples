640ef7faf596dd997bd1b9f8c7be1696f224ed4f
Victor H. Faria <victorfaria1590@gmail.com>
2022-09-04 20:33:00 -0300

    Using and customizing HttpLogging.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 using Platform;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Ref https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-logging/?view=aspnetcore-6.0
+builder.Services.AddHttpLogging(opts => {
+ opts.LoggingFields = 
+ HttpLoggingFields.RequestMethod | 
+ HttpLoggingFields.RequestPath | 
+ HttpLoggingFields.ResponseStatusCode;
+});
+
 var app = builder.Build();
 
-app.Logger.LogDebug("Pipeline configuration starting");
+app.UseHttpLogging();
 
-var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline"); // Custom Logger.
-
-logger.LogDebug("Pipeline configuration starting");
+//var logger = app.Services
+// .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+//logger.LogDebug("Pipeline configuration starting");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
-logger.LogDebug("Pipeline configuration complete");
-
-app.Logger.LogDebug("Pipeline configuration complete");
+//logger.LogDebug("Pipeline configuration complete");
 
 app.Run();
